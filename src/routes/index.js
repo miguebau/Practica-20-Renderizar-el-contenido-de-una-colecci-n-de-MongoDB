4b33520ac667103ed22a00f80a0c98ffd2440d2b
miguebau <mbautista7@ucol.mx>
2023-05-30 09:24:23 -0600

    prac21

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-let express = require('express'); //iniciamos express
-let router = express.Router(); //Usamos el Router de express 
-let Person = require('../models/person');
-
-router.get('/', async(req, res) => {
-    res.send("<h1>App en linea</h1>")
-});
-
-module.exports = router;
--- src/routes/person.js
+++ src/routes/person.js
@@ -7,15 +7,21 @@ router.post('/personJson', express.json({ type: '*/*' }), (req, res) => {
     console.log(`Nombre: ${req.body.Nombre}\nApellido: ${req.body.Apellido}`) //Mostramos la informacion 
 });
 
-router.get('/testJson', (req, res) => {
-    res.render('testJson');
-})
-
-router.get('/gente', async(req, res) => {
-    let data = await Person.find({});
+router.get('/', async(req, res) => {
+    let data = await Person.find({}); // Crear una consulta a mongo
     res.render('index', { data })
-
 });
+
+
+router.get('/student', (req, res) => {
+    res.render('student'); // creamos vista
+});
+
+router.post('/addStudent', (req, res) => {
+    const persona = Person({ "Nombre": req.body.nombre, "Edad": req.body.edad, "Nss": req.body.nss, "TpSangre": req.body.tpSangre })
+
+    persona.save().then(() => { res.redirect('/'); }); // Con este comando podemos guardar el nuevo documento en mongoDB atlas
+})
 
 
 module.exports = router; //exportamos el router
